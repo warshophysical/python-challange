b61d32ef3e53e9bf000f0b6bb8b0c8b3f4b55222
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-05 13:10:46 -0500

    Greatest Increase and Decrease

--- PyBank/mainapp.py
+++ PyBank/mainapp.py
@@ -19,57 +19,68 @@ def ReadReturnCSV(path):
     return dictAB
     
 # The function returns length for iterable objects
-def ReturnLengthIterableItem(it):
-    itLenght=len(it)
-    return(itLenght)
 
-# Total number of months
+
+# MAIN DICTIONARY WHICH WAS CONVERTED FROM CSV
+#####
 dictBudget=ReadReturnCSV(csv_path)
-totalNumberMonth=ReturnLengthIterableItem(dictBudget)
-print("Total Number of Months: "+str(totalNumberMonth))
+#####
 
-
-# Total budget
-def TotalValueDict(dicta):
+# Function which gives total value
+def TotalValueDict(budgetDictionary):
     total=0
-    for key,value in dicta.items():
+    for key,value in budgetDictionary.items():
         total=total+value
 
     return total
 
+
+# The function which gives average change
+def AverageValueDict(budgetDictionary):
+    dictBudgetValues=list(budgetDictionary.values())
+    
+    listBudgetChange=[]
+    for i in range(len(dictBudgetValues)-1):
+        listBudgetChange.append(dictBudgetValues[i+1]-dictBudgetValues[i])
+
+    totalChangeVolume=0
+    numberChanged=0
+    for i in listBudgetChange:
+        totalChangeVolume=totalChangeVolume+i
+        numberChanged=numberChanged+1
+
+
+    averageChange=totalChangeVolume/numberChanged
+    averageChange=round(averageChange,2)
+
+    return averageChange
+
+
+
+# Function calculates greatest increase of change
+def BiggestIncrease(budgetDictionary):
+    sortedbudgetDictionary=sorted(budgetDictionary.items(), key=lambda x: x[1])
+    return sortedbudgetDictionary[-1]
+
+# Function calculates greatest decrease of change
+def BiggestDecrease(budgetDictionary):
+    sortedbudgetDictionary=sorted(budgetDictionary.items(), key=lambda x: x[1])
+    return sortedbudgetDictionary[1]
+
+totalNumberMonth=len(dictBudget)
 budgetTotal=TotalValueDict(dictBudget)
+averageChange=AverageValueDict(dictBudget)
+budgetBiggestIncrease= BiggestIncrease(dictBudget)
+budgetBiggestDecrease= BiggestDecrease(dictBudget)
+
+
+print("#########################")
+print("FINANCIAL ANALYSIS")
+print("Total Months: "+str(totalNumberMonth))
 print("Budget Total: $"+str(budgetTotal))
-
-
-# Code calculates budget change monthly
-# The change is listed
-dictBudgetValues=list(dictBudget.values())
-listBudgetChange=[]
-for i in range(len(dictBudgetValues)-1):
-    listBudgetChange.append(dictBudgetValues[i+1]-dictBudgetValues[i])
-
-# The code calculate average change monthly
-
-totalChangeVolume=0
-numberChanged=0
-for i in listBudgetChange:
-    totalChangeVolume=totalChangeVolume+i
-    numberChanged=numberChanged+1
-
-averageChange=totalChangeVolume/numberChanged
-averageChange=round(averageChange,2)
 print("Average Budget Change: $"+str(averageChange))
-
-
-
-
-
-
-
-
-
-
-
+print("Biggest Increase: "+str(budgetBiggestIncrease))
+print("Biggest Decrease: "+str(budgetBiggestDecrease))
 
 
 # Dictionary
@@ -135,8 +146,55 @@ for key,value in dictBudget.items():
 
 
 dictBudget2010=dict(budget2010)
+dictBudget2011=dict(budget2011)
+dictBudget2012=dict(budget2012)
+dictBudget2013=dict(budget2013)
+dictBudget2014=dict(budget2014)
+dictBudget2015=dict(budget2015)
+dictBudget2016=dict(budget2016)
+dictBudget2017=dict(budget2017)
+print("---------------")
+budgetBiggestIncrease2010= BiggestIncrease(dictBudget2010)
+budgetBiggestDecrease2010= BiggestDecrease(dictBudget2010)
+print("Biggest Increase of 2010: "+str(budgetBiggestIncrease2010))
+print("Biggest Decrease of 2010: "+str(budgetBiggestDecrease2010))
+print("---------------")
+budgetBiggestIncrease2011= BiggestIncrease(dictBudget2011)
+budgetBiggestDecrease2011= BiggestDecrease(dictBudget2011)
+print("Biggest Increase of 2011: "+str(budgetBiggestIncrease2011))
+print("Biggest Decrease of 2011: "+str(budgetBiggestDecrease2011))
+print("---------------")
+budgetBiggestIncrease2012= BiggestIncrease(dictBudget2012)
+budgetBiggestDecrease2012= BiggestDecrease(dictBudget2012)
+print("Biggest Increase of 2012: "+str(budgetBiggestIncrease2012))
+print("Biggest Decrease of 2012: "+str(budgetBiggestDecrease2012))
+print("---------------")
+budgetBiggestIncrease2013= BiggestIncrease(dictBudget2013)
+budgetBiggestDecrease2013= BiggestDecrease(dictBudget2013)
+print("Biggest Increase of 2013: "+str(budgetBiggestIncrease2013))
+print("Biggest Decrease of 2013: "+str(budgetBiggestDecrease2013))
+print("---------------")
+budgetBiggestIncrease2014= BiggestIncrease(dictBudget2014)
+budgetBiggestDecrease2014= BiggestDecrease(dictBudget2014)
+print("Biggest Increase of 2014: "+str(budgetBiggestIncrease2014))
+print("Biggest Decrease of 2014: "+str(budgetBiggestDecrease2014))
+print("---------------")
+budgetBiggestIncrease2015= BiggestIncrease(dictBudget2015)
+budgetBiggestDecrease2015= BiggestDecrease(dictBudget2015)
+print("Biggest Increase of 2015: "+str(budgetBiggestIncrease2015))
+print("Biggest Decrease of 2015: "+str(budgetBiggestDecrease2015))
+print("---------------")
+budgetBiggestIncrease2016= BiggestIncrease(dictBudget2016)
+budgetBiggestDecrease2016= BiggestDecrease(dictBudget2016)
+print("Biggest Increase of 2016: "+str(budgetBiggestIncrease2016))
+print("Biggest Decrease of 2016: "+str(budgetBiggestDecrease2016))
+print("---------------")
+budgetBiggestIncrease2017= BiggestIncrease(dictBudget2017)
+budgetBiggestDecrease2017= BiggestDecrease(dictBudget2017)
+print("Biggest Increase of 2017: "+str(budgetBiggestIncrease2017))
+print("Biggest Decrease of 2017: "+str(budgetBiggestDecrease2017))
 
-#print(dictBudget2010)
+
 
 
 
